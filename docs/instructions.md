26a73668a037fad8b83b5b56a97dc6d49dbebad5
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-24 20:21:00 +0200

    feat: initialize database

---
+++ docs/instructions.md
@@ -0,0 +1,22 @@
+# Instructions
+
+todo
+
+### Table of Contents
+- [Setup](#setup)
+
+
+## Setup
+
+1. Clone the [repo](https://github.com/immone/ohtuprojekti-2022/tree/dev)
+```bash
+git clone git@github.com:immone/ohtuprojekti-2022.git
+````
+2. Install dependencies
+```bash
+poetry install
+````
+3. Build
+```bash
+poetry run python3 src/build.py
+````
---
+++ src/initialize_database.py
@@ -0,0 +1,67 @@
+from database_connection import get_database_connection
+
+
+def drop_tables(connection):
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        drop table if exists reference;
+    """)
+
+    cursor.execute("""
+        drop table if exists reference_author;
+    """)
+
+    cursor.execute("""
+        drop table if exists author;
+    """)
+
+    connection.commit()
+
+
+def create_tables(connection):
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        create table author (
+            author_id integer not null,
+            name text not null,
+            unique (name),
+            primary key (author_id)
+        );
+    """)
+
+    cursor.execute("""
+        create table reference (
+            reference_id text not null,
+            title text not null,
+            year integer not null,
+            publisher text not null,
+            primary key (reference_id)
+        );
+    """)
+
+    cursor.execute("""
+         create table reference_author (
+            reference_id text,
+            author_id integer,
+            foreign key (reference_id) references reference(reference_id),
+            foreign key (author_id) references author(author_id)
+        );
+    """)
+
+    connection.commit()
+
+
+def initialize_database():
+
+    connection = get_database_connection()
+
+    drop_tables(connection)
+    create_tables(connection)
+
+
+if __name__ == "__main__":
+    initialize_database()
