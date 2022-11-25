0df8aed3cf6503bc792c21805b6d5ddee3334473
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-25 15:33:23 +0200

    feat: reference entity

---
+++ src/entities/reference.py
@@ -0,0 +1,36 @@
+class Reference:
+    def __init__(self, reference_id: str, authors: list, title: str, year: int, publisher: str):
+        self.__reference_id = reference_id
+        self.__authors = authors
+        self.__title = title
+        self.__year = year
+        self.__publisher = publisher
+
+    @property
+    def reference_id(self):
+        return self.__reference_id
+    
+    @property
+    def authors(self):
+        return self.__authors
+    
+    @property
+    def title(self):
+        return self.__title
+    
+    @property
+    def year(self):
+        return self.__year
+    
+    @property
+    def publisher(self):
+        return self.__publisher
+    
+    def to_dict(self):
+        return {
+            "reference_id": self.reference_id,
+            "title": self.title,
+            "year": self.year,
+            "publisher": self.publisher,
+            "authors": self.authors
+        }
