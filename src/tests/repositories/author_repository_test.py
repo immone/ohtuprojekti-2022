05425898bfb78ace309a3060b688e0b976c7609e
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-29 18:39:43 +0200

    test: unittests on repositories

---
+++ src/tests/repositories/author_repository_test.py
@@ -0,0 +1,34 @@
+import unittest
+from database_connection import get_database_connection
+from repositories.author_repository import AuthorRepository
+from repositories.reference_repository import ReferenceRepository
+
+
+class AuthorRepositoryTest(unittest.TestCase):
+    def setUp(self):
+        self.connection = get_database_connection()
+        ReferenceRepository().delete_all()
+        self.author_repository = AuthorRepository(self.connection)
+        self.author_id = self.author_repository.post("Test Author")
+
+    def test_author_repository_exits(self):
+        self.assertIsNotNone(self.author_repository)
+
+    def test_get(self):
+        self.assertEqual(
+            self.author_id, self.author_repository.get("Test Author"))
+
+    def test_get_non_existent_author(self):
+        self.assertIsNone(self.author_repository.get("Non Existent Author"))
+
+    def test_post_author(self):
+        self.assertEqual(2, self.author_repository.post("second author"))
+        self.assertEqual(2, self.author_repository.get("second author"))
+
+    def test_post_identical_author(self):
+        self.assertEqual(1, self.author_repository.post("Test Author"))
+        self.assertEqual(1, self.author_repository.get("Test Author"))
+
+    def test_delete(self):
+        self.author_repository.delete(self.author_id)
+        self.assertIsNone(self.author_repository.get("Test Author"))
---
+++ src/tests/repositories/reference_author_repository_test.py
@@ -0,0 +1,67 @@
+import unittest
+from database_connection import get_database_connection
+from repositories.reference_author_repository import ReferenceAuthorRepository
+from repositories.author_repository import AuthorRepository
+from repositories.reference_repository import ReferenceRepository
+from entities.reference import Reference
+
+
+class ReferenceAuthorRepositoryTest(unittest.TestCase):
+    def setUp(self):
+        self.connection = get_database_connection()
+        ReferenceRepository().delete_all()
+        self.author_repository = AuthorRepository(self.connection)
+        self.reference_author_repository = ReferenceAuthorRepository(
+            self.connection, self.author_repository)
+        self.mock_reference = Reference(
+            reference_id="1",
+            authors=["Test Author 1", "Test Author 2"],
+            title="Test Title",
+            year="2020",
+            publisher="Test Publisher"
+        )
+        self.reference_author_repository.post(self.mock_reference)
+
+    def test_reference_author_repository_exits(self):
+        self.assertIsNotNone(self.reference_author_repository)
+
+    def test_get(self):
+        reference_authors = self.reference_author_repository.get(
+            self.mock_reference.reference_id)
+        self.assertEqual(self.mock_reference.authors, reference_authors)
+
+    def test_get_non_existent_reference(self):
+        reference_authors = self.reference_author_repository.get(
+            "Non Existent Reference")
+        self.assertEqual([], reference_authors)
+
+    def test_post_reference(self):
+        mock_reference = Reference(
+            reference_id="2",
+            authors=["Test Author 1", "Test Author 2"],
+            title="Test Title",
+            year="2020",
+            publisher="Test Publisher"
+        )
+        self.reference_author_repository.post(mock_reference)
+        reference_authors = self.reference_author_repository.get(
+            mock_reference.reference_id)
+        self.assertEqual(mock_reference.authors, reference_authors)
+        self.assertEqual(1, self.author_repository.get("Test Author 1"))
+
+    def test_post_identical_reference_causes_error(self):
+        with self.assertRaises(Exception):
+            self.reference_author_repository.post(self.mock_reference)
+
+    def test_delete(self):
+        self.reference_author_repository.delete(
+            self.mock_reference.reference_id)
+        reference_authors = self.reference_author_repository.get(
+            self.mock_reference.reference_id)
+        self.assertEqual([], reference_authors)
+
+    def test_delete_non_existent_reference(self):
+        self.reference_author_repository.delete("Non Existent Reference")
+        reference_authors = self.reference_author_repository.get(
+            self.mock_reference.reference_id)
+        self.assertEqual(self.mock_reference.authors, reference_authors)
