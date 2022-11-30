30eda6b05c409893dcb5d8fe4bb7ff57521930ac
Antti Immonen <antti.h.immonen@aalto.fi>
2022-11-30 09:56:25 +0200

    test: add tests for delete and print

---
+++ src/tests/delete_test.py
@@ -0,0 +1,56 @@
+import unittest
+from unittest.mock import Mock, patch
+from repositories.reference_repository import ReferenceRepository
+from commands.add import Add
+from io import StringIO
+from commands.delete import Delete
+from entities.reference import Reference
+
+class TestDelete(unittest.TestCase):
+    def setUp(self):
+        self.repository_mock = Mock()
+        self.mock_reference = Reference(
+            reference_id="id",
+            authors=["author"],
+            title="value",
+            year=1234,
+            publisher="value"
+        )
+        self.repository_mock.post(self.mock_reference)
+        self.io_mock = Mock()
+
+
+    def test_delete_method_of_repository_called(self):
+        self.io_mock.read.side_effect = ["id"]
+        delete = Delete(self.repository_mock, self.io_mock)
+        delete.run()
+        self.repository_mock.delete.assert_called_with("id")
+
+    def test_delete_when_id_doesnt_exist(self):
+        self.reference_repository = ReferenceRepository()
+        self.mock_reference = Reference(
+            reference_id="id",
+            authors=["author"],
+            title="field",
+            year=1234,
+            publisher="field"
+        )
+        self.reference_repository.post(self.mock_reference)
+        self.io_mock.read.side_effect = ["id2"]
+        delete = Delete(self.reference_repository, self.io_mock)
+        self.assertRaises(StopIteration, delete.run)
+
+    def test_when_exists(self):
+        self.reference_repository = ReferenceRepository()
+        self.mock_reference = Reference(
+            reference_id="id",
+            authors=["author"],
+            title="field",
+            year=1234,
+            publisher="field"
+        )
+        self.io_mock.read.side_effect = ["id"]
+        delete = Delete(self.reference_repository, self.io_mock)
+        delete.run()
+        self.assertFalse(self.reference_repository.id_exists("id"))
+        self.assertRaises(StopIteration, delete.run)
