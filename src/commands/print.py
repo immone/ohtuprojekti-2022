8a0210a8e5c19c12e201c465afa7bde399bfe020
Antti Immonen <antti.h.immonen@aalto.fi>
2022-11-30 09:55:26 +0200

    fix: change rprints to use io class

--- src/commands/print.py
+++ src/commands/print.py
@@ -2,18 +2,19 @@ import sys
 from entities.reference import Reference
 
 class Print():
-    def __init__(self, repository):
+    def __init__(self, repository, io):
         self.repository = repository
+        self.io = io
 
-    def list(self):
-        print("Printing all references in the database..")
+    def run(self):
+        self.io.write("Printing all references in the database..")
 
         all_references = self.repository.get_all()
         if len(all_references) == 0:
-            print("There are no references to be printed.")
+            self.io.write("There are no references to be printed.")
         else:
             for ref in all_references:
-                print("Author(s): ", ref.__authors)
-                print("Title: ", ref.__title)
-                print("Published in: ", ref.__year)
-                print("Published by: ", ref.__publisher, "\n")
+                self.io.write("Author(s): ", ref.__authors)
+                self.io.write("Title: ", ref.__title)
+                self.io.write("Published in: ", ref.__year)
+                self.io.write("Published by: ", ref.__publisher, "\n")
