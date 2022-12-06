73851b45fe48fe1877ced435bb59ece66b1d167f
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-12-06 20:36:13 +0200

    feat(console_io): if the empty_msg is not provided, the query is considered optional

--- src/console_io.py
+++ src/console_io.py
@@ -1,14 +1,13 @@
 class ConsoleIO:
-    def read(self, prompt, empty_msg):
+    def read(self, prompt, empty_msg=None):
         query = input(prompt)
-        while len(query) == 0:
-            print(empty_msg)
-            query = input(prompt)
+
+        if not empty_msg is None:
+            while len(query) == 0:
+                print(empty_msg)
+                query = input(prompt)
 
         return query
-
-    def read_opt(self, prompt):
-        return input(prompt)
 
     def write(self, data):
         print(data)
