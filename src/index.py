26aa62234bf411734e95bc1cfc03dc0034a9405b
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-12-06 21:53:25 +0200

    feat: add simple search command

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import sys
 from commands.add import Add
 from commands.translator import Translator
+from commands.search import Search
 from repositories.reference_repository import ReferenceRepository
 from services.reference_service import ReferenceService
 from console_io import ConsoleIO
@@ -11,21 +12,26 @@ def main():
     if len(args) == 0:
         io = ConsoleIO()
         while True:
-            command = io.read("Enter command: ", "Please provide a valid command")
+            command = io.read("Enter command: ", "Please provide a command")
             if command == "add":
                 add()
-            if command == "bibtex":
+            elif command == "bibtex":
                 bibtex()
-            if command == "help":
+            elif command == "help":
                 list_commands(io)
-            if command == "exit":
+            elif command == "search":
+                search()
+            elif command == "exit":
                 return
+            else:
+                list_commands(io)
 
     if args[0] == "add":
         add()
     elif args[0] == "bibtex":
         bibtex()
-
+    elif args[0] == "search":
+        search()
 
 def list_commands(io):
     io.write("Give: ")
@@ -35,13 +41,17 @@ def list_commands(io):
 
 
 def add():
-    add = Add(ReferenceService(),ConsoleIO())
+    add = Add(ReferenceService(), ConsoleIO())
     add.run()
 
 
 def bibtex():
     translator = Translator(ReferenceService(), ConsoleIO())
     translator.run()
+
+
+def search():
+    search = Search(ReferenceService(), ConsoleIO())
 
 
 if __name__ == "__main__":
