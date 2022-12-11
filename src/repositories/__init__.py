461cbb12808c9591987df6c2661a6eab230141dc
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-12-11 13:15:50 +0200

    refactor: accommodate new schema

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,5 +1,1 @@
-from .author_repository import AuthorRepository
-from .reference_author_repository import ReferenceAuthorRepository
 from .reference_repository import ReferenceRepository
-from .tag_repository import TagRepository
-from .reference_tag_repository import ReferenceTagRepository
