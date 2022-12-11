6962f3fef11727dd7f8429a0fe1e254171c7b8fc
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-12-11 13:18:44 +0200

    test: updated service test

--- src/tests/services/reference_service_test.py
+++ src/tests/services/reference_service_test.py
@@ -1,70 +1,172 @@
 import unittest
 from services import ReferenceService
-from entities import Reference
 
 
 class ReferenceServiceTest(unittest.TestCase):
     def setUp(self):
         self.service = ReferenceService()
         self.service.delete_all()
-        self.fake_reference = Reference(
-            reference_id="1",
-            authors=["test_author", "test_author_2"],
-            title="Moomie",
-            year=2020,
-            publisher="Dotterbolaget",
-            tags=["test_tag", "test_tag_2"]
-        )
-        self.fake_reference_2 = Reference(
-            reference_id="2",
-            authors=["test_author_2", "test_author_3"],
-            title="Winnie the Pooh",
-            year=1999,
-            publisher="Epic Games",
-            tags=["test_tag_2", "test_tag_3"]
-        )
+        self.article = {
+            "reference_id": "1",
+            "type": "article",
+            "author": ["P. J. Cohen"],
+            "title": "The independence of the continuum hypothesis",
+            "journal": "Proceedings of the National Academy of Sciences",
+            "year": "1963",
+            "volume": "50",
+            "number": "6",
+            "pages": "1143--1148",
+            "tag": ["test_tag1", "test_tag_2"]
+        }
+        self.book = {
+            "reference_id": "2",
+            "type": "book",
+            "author": ["Leonard Susskind", "George Hrabovsky"],
+            "title": "Classical mechanics: the theoretical minimum",
+            "publisher": "Penguin Random House",
+            "year": "2014",
+            "tag": ["test_tag2", "test_tag_3"]
+        }
+        self.inproceedings = {
+            "reference_id": "3",
+            "type": "inproceedings",
+            "author": ["Holleis, Paul", "Wagner, Matthias", "Koolwaaij, Johan"],
+            "title": "Studying mobile context-aware social services in the wild",
+            "booktitle": "Proc. of the 6th Nordic Conf. on Human-Computer Interaction",
+            "series": "NordiCHI",
+            "year": "2010",
+            "pages": "207--216",
+            "publisher": "ACM",
+            "tag": ["test_tag3", "test_tag_4"]
+        }
+        self.misc = {
+            "reference_id": "4",
+            "type": "misc",
+            "author": ["J. R. S. Blair"],
+            "title": "The 3x3x3 Rubik's Cube",
+            "howpublished": "http://www.cubewhiz.com/",
+            "year": "2009",
+            "tag": ["test_tag4", "test_tag_5"]
+        }
 
-    def test_id_exists(self):
-        self.service.post(self.fake_reference)
+    def test_can_post_article(self):
+        self.service.post(self.article)
         self.assertTrue(self.service.id_exists(1))
 
+    def test_can_post_book(self):
+        self.service.post(self.book)
+        self.assertTrue(self.service.id_exists(2))
+
+    def test_can_post_inproceedings(self):
+        self.service.post(self.inproceedings)
+        self.assertTrue(self.service.id_exists(3))
+
+    def test_can_post_misc(self):
+        self.service.post(self.misc)
+        self.assertTrue(self.service.id_exists(4))
+
+    def test_id_exists(self):
+        self.service.post(self.article)
+        self.assertTrue(self.service.id_exists(1))
+
+    def test_invalid_id_does_not_exist(self):
+        self.service.post(self.article)
+        self.assertFalse(self.service.id_exists(2))
+
+    def test_get_type(self):
+        self.service.post(self.article)
+        self.assertEqual("article", self.service.get_type(1))
+
+    def test_get_type_invalid_id(self):
+        self.service.post(self.article)
+        self.assertEqual(None, self.service.get_type(2))
+
+    def test_get_fields(self):
+        self.service.post(self.article)
+        fields = sorted(["author", "title", "journal", "year",
+                        "volume", "number", "pages", "tag"])
+        self.assertEqual(fields, self.service.get_fields("article"))
+
+    def test_get_fields_invalid_type(self):
+        self.service.post(self.article)
+        self.assertEqual([], self.service.get_fields("invalid_type"))
+
     def test_get(self):
-        self.service.post(self.fake_reference)
-        self.assertEqual(self.fake_reference.to_dict(),
-                         self.service.get(1).to_dict())
+        self.service.post(self.article)
+        reference = self.service.get(1)
+        del reference["reference_id"]
+        del reference["type"]
+        self.assertEqual(self.article, reference)
+
+    def test_get_invalid_id(self):
+        self.service.post(self.article)
+        self.assertEqual({}, self.service.get(2))
 
     def test_get_all(self):
-        self.service.post(self.fake_reference)
-        self.service.post(self.fake_reference_2)
-        self.assertEqual([self.fake_reference.to_dict(), self.fake_reference_2.to_dict()], [
-                         reference.to_dict() for reference in self.service.get_all()])
+        self.service.post(self.article)
+        self.service.post(self.book)
+        self.service.post(self.inproceedings)
+        self.service.post(self.misc)
+        references = self.service.get_all()
+        for i in references:
+            del i["reference_id"]
+            del i["type"]
+        self.assertEqual(4, len(references))
+        self.assertEqual(self.article, references[0])
+        self.assertEqual(self.book, references[1])
+        self.assertEqual(self.inproceedings, references[2])
+        self.assertEqual(self.misc, references[3])
+
+    def test_get_all_empty(self):
+        self.assertEqual([], self.service.get_all())
 
     def test_post(self):
-        self.service.post(self.fake_reference)
-        self.assertEqual(self.fake_reference.to_dict(),
-                         self.service.get(1).to_dict())
+        self.service.post(self.article)
+        article = self.service.get(1)
+        del article["reference_id"]
+        del article["type"]
+        self.assertEqual(self.article, article)
+
+    def test_post_invalid_type(self):
+        self.article["type"] = "invalid_type"
+        with self.assertRaises(Exception):
+            self.service.post(self.article)
+        self.assertFalse(self.service.id_exists(1))
+
+    def test_post_invalid_field(self):
+        self.article["invalid_field"] = "invalid_value"
+        article = self.service.get(1)
+        self.assertNotEqual(self.article, article)
 
     def test_put(self):
-        self.service.post(self.fake_reference)
-        fake_reference = Reference(
-            reference_id="1",
-            authors=["test_author_2", "test_author_3"],
-            title="Winnie the Pooh",
-            year=1999,
-            publisher="Epic Games",
-            tags=["test_tag_66", "test_tag_99"]
-        )
-        self.service.put(fake_reference)
-        self.assertEqual(fake_reference.to_dict(),
-                         self.service.get(1).to_dict())
+        self.service.post(self.article)
+        self.service.put(1, "title", "new_title")
+        self.assertEqual("new_title", self.service.get(1)["title"])
+
+    def test_put_invalid_id(self):
+        self.service.post(self.article)
+        self.service.put(2, "title", "new_title")
+        self.assertEqual(self.article["title"], self.service.get(1)["title"])
+
+    def test_put_invalid_field(self):
+        self.service.post(self.article)
+        self.service.put(1, "invalid_field", "new_value")
+        self.assertEqual(self.article["title"], self.service.get(1)["title"])
 
     def test_delete(self):
-        self.service.post(self.fake_reference)
+        self.service.post(self.article)
         self.service.delete(1)
         self.assertFalse(self.service.id_exists(1))
 
+    def test_delete_invalid_id(self):
+        self.service.post(self.article)
+        self.service.delete(2)
+        self.assertTrue(self.service.id_exists(1))
+
     def test_delete_all(self):
-        self.service.post(self.fake_reference)
-        self.service.post(self.fake_reference_2)
+        self.service.post(self.article)
+        self.service.post(self.book)
+        self.service.post(self.inproceedings)
+        self.service.post(self.misc)
         self.service.delete_all()
         self.assertEqual([], self.service.get_all())
