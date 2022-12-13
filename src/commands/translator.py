08a9e638447bc5aa3b7aa533b87a749d74e9edef
helmer <helmer.lauronen@gmail.com>
2022-12-13 13:34:33 +0200

    fix: refactoring translator and adding testing

--- src/commands/translator.py
+++ src/commands/translator.py
@@ -8,33 +8,37 @@ class Translator:
         self.command_io = io
 
     def run(self):
-        self.__print_all()
-
-    def __print_all(self):
         self.command_io.write("Trying to print all references...")
         try:
             reference_list = self.service.get_all()
         except:
             sys.exit("\n A database error occurred. Failed to load references.")
+        self.__print_all(reference_list)
 
-        for ref in reference_list:
-            authors = ""
-            for i in range(0, len(ref.authors)):
-                if i == len(ref.authors)-1:
-                    authors += (" " + ref.authors[i])
-                elif i == 0:
-                    authors += (ref.authors[i] + ",")
-                else:
-                    authors += (" " + ref.authors[i] + ",")
-            self.__print_ref(ref, authors)
+    def __print_all(self, references):
 
-    def __print_ref(self, ref, authors):
+        for ref in references:
+            self.__print_ref(ref)
+
+    def __print_ref(self, ref):
+        authors = ""
+        for i in range(0, len(ref.authors)):
+            if i == len(ref.authors)-1:
+                authors += (" " + ref.authors[i])
+            elif i == 0:
+                authors += (ref.authors[i] + ",")
+            else:
+                authors += (" " + ref.authors[i] + ",")
+                
         self.command_io.write("@book{" + ref.reference_id + ",")
         self.command_io.write("  author    = {" + authors + "}, ")
         self.command_io.write("  title     = {" + ref.title + "},")
         self.command_io.write("  year      = {" + str(ref.year) + "},")
         self.command_io.write("  publisher = {" + ref.publisher + "},")
         self.command_io.write("}")
+    
+    def __print_article(self, ref):
+        pass
 
     def __query_saved_reference_id(self):
         while True:
