aa6132b42506425ce3fddb89e900f86a34a1a399
Antti Immonen <antti.h.immonen@aalto.fi>
2022-12-15 06:39:48 +0200

    feat: filter listing according to tags

--- src/commands/list.py
+++ src/commands/list.py
@@ -8,6 +8,18 @@ class List():
         self.io = io
 
     def run(self):
+        tag_filter = self.io.read("Would you like to filter the listing according to some tags? (y/n) ",
+                                  "Please indicate your answer")
+        while tag_filter not in ["y", "n", "yes", "no"]:
+            print(tag_filter)
+            self.io.read("Would you like to filter the listing according to some tags? (y/n) ",
+                         "Please indicate your answer")
+        if tag_filter in ["n", "no"]:
+            self.__no_tag()
+        else:
+            self.__tag()
+
+    def __no_tag(self):
         all_references = self.service.get_all()
         if len(all_references) == 0:
             self.io.write("There are no references to be listed.")
@@ -18,6 +30,44 @@ class List():
                 for val in ref:
                     self.io.write(self.__match_key_to_io_write(val, ref))
                 self.io.write("")
+
+    def __tag(self):
+        tags = self.io.read("Please enter the tags you would like search under (delimited by semicolons)",
+                            "Please enter a nonempty tag")
+        tags_parsed = [t.strip() for t in tags.split(";")]
+
+        operation = self.io.read("Would you like the search to include matches that contain all or any of the given tags? (all/any) ",
+                            "Please indicate your answer")
+
+        # union or intersection of search words
+        while operation not in ["all", "any"]:
+            self.io.read(
+                "Would you like the search to include matches that contain all or any of the given tags? (all/any)",
+                "Please indicate your answer")
+            
+        if operation == "any":
+            tag_refs = []
+            for tag in tags_parsed:
+                tag_refs = tag_refs + self.service.get_by_tag(tag)
+        else:
+            tag_refs = []
+            for tag in tags_parsed:
+                candidate_refs = self.service.get_by_tag(tag)
+                for candidate in candidate_refs:
+                    print(candidate["tag"], tags_parsed)
+                    if all(tag in candidate["tag"] for tag in tags_parsed) and candidate not in tag_refs:
+                        tag_refs.append(candidate)
+
+        if len(tag_refs) == 0:
+            self.io.write("There are no references to be listed.")
+        else:
+            self.io.write(f"Found a total number of {len(tag_refs)} references under the given tag(s).\n")
+            self.io.write("Printing all found references under the given tag(s)..\n")
+            for ref in tag_refs:
+                for val in ref:
+                    self.io.write(self.__match_key_to_io_write(val, ref))
+                self.io.write("")
+
 
     def __match_key_to_io_write(self, key, ref):
         match key:
