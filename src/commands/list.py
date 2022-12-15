cb8f94f3c51ef635ed2093deee4a9d6ec63103ed
Antti Immonen <antti.h.immonen@aalto.fi>
2022-12-15 05:56:56 +0200

    update: list command

--- src/commands/list.py
+++ src/commands/list.py
@@ -1,4 +1,5 @@
 from services.reference_service import ReferenceService
+import sys
 
 class List():
     def __init__(self, repository, io):
@@ -14,9 +15,35 @@ class List():
             self.io.write(f"Found a total number of {len(all_references)} references.\n")
             self.io.write("Printing all references in the database..\n")
             for ref in all_references:
-                self.io.write(f"Title: {ref.title} \n"
-                              f"Author(s): {', '.join(ref.authors)} \n"
-                              f"Published in: {ref.year} \n"
-                              f"Published by: {ref.publisher} \n"
-                              f"Reference ID: {ref.reference_id} \n"
-                              f"Tags: {', '.join(ref.tags)}\n")
+                for val in ref:
+                    self.io.write(self.__match_key_to_io_write(val, ref))
+                self.io.write("")
+
+    def __match_key_to_io_write(self, key, ref):
+        match key:
+            case "type":
+                return f"Reference type: {ref['type']}"
+            case "title":
+                return f"Title: {ref['title']}"
+            case "author":
+                return f"Author(s): {', '.join(ref['author'])}"
+            case "booktitle":
+                return f"Book title: {ref['booktitle']}"
+            case "series":
+                return f"Series: {ref['series']}"
+            case "pages":
+                return f"Pages: {ref['pages']}"
+            case "year":
+                return f"Published in: {ref['year']}"
+            case "publisher":
+                return f"Published by: {ref['publisher']}"
+            case"howpublished":
+                return f"How published (notes): {ref['howpublished']}"
+            case "address":
+                return f"Address: {ref['address']}"
+            case "reference_id":
+                return f"Reference ID: {ref['reference_id']}"
+            case "tag":
+                return f"Tags: {', '.join(ref['tag'])}"
+            case _:
+                pass
